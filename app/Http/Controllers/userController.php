88dea042cb52e5021dc8a35b4f4448ba3b490dc5
CristianCODE123 <starblum123@gmail.com>
2023-02-03 11:27:31 -0500

    first commit back separted

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:255',
+            'email' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return json_encode(["error" => "falla"]);
+        }
+        User::create($request->all()); 
+        return  json_encode(["creado" => "cliente creado correctamente"]);   
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if(User::where('id',$id)->exists()){
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = $request->password;
+            $user->save();
+            return json_encode(["Actualizado" => "el usuario se ha actualizado correctamente"]);
+        }else{
+            return json_encode(["No actualizado" => "no se ha actualizado"]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if(User::where('id',$id)->exists()){
+            $user = User::find($id);
+            $user->delete();
+
+            return json_encode(["Borrado"=>"datos borrados"]);
+        }else{
+            return json_encode(["no borrado" => "datos no borrads"]);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userController;
+use App\Http\Resources\UserResource;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('/usuario/{id}',function ($id){
+    return new UserResource(User::findOrFail($id));
+});
+Route::get('/usuario',function (){
+    return  UserResource::collection(User::all());
+});
+
+Route::post('/usuario',[userController::class,'store']);
+Route::put('/usuario/{id}', [userController::class,'update']);
+Route::delete("/usuario/{id}", [userController::class,'destroy']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
